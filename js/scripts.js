f50b5e6ab0f85cf713f03be8c6646d7b6f79b820
Adhal Ahmed <P17151661@my365.dmu.ac.uk>
2020-02-19 11:10:03 +0000

    forst commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const menuToggle = document.getElementById('menu-toggle');
+
+const menuNav = document.getElementById('menu-nav');
+
+const toggleMenu = () =>
+{
+  console.log("called toggleMenu")
+  menuNav.classList.toggle("menu-toggle");
+}
+
+menuToggle.addEventListener("click", toggleMenu);
